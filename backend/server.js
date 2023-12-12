b16a950d2a79e08749c821a48c4b606367bbe9e9
jdelmundo23 <joshdelm@gmail.com>
2023-12-12 18:50:34 -0500

    added route for getting random pkmn

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,25 @@ app.get('/true', async (req, res) => {
     res.status(200).json({'working' : 'true'});
 })
 
+app.get('/random', async (req, res) => {
+    const resAll = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=507');
+    const dataAll = await resAll.json();
+    const randInt = Math.floor(Math.random() * 507)
+    const randPkmn = dataAll.results[randInt];
+    const resTarg = await fetch(`https://pokeapi.co/api/v2/pokemon/${randPkmn.name}`)
+    const dataTarg = await resTarg.json();
+    // const resSpec = await fetch(dataTarg.species.url)
+    // const dataSpec = await resSpec.json();
+    // const resChain = await fetch(dataSpec.evolution_chain.url);
+    // const dataChain = await resChain.json();
+    // console.log(dataChain)
+    res.status(200).json(dataTarg);
+})
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(4000, () => {
-            console.log('listening on port 4000')
+            console.log('listening on port 4000 and connected to db')
         })
     })
     .catch((error) => {
