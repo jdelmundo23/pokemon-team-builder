804caccbb37e7bdd6d1ec9d4bf551b0cf86c358b
jdelmundo23 <joshdelm@gmail.com>
2023-12-12 16:16:07 -0500

    added mongo atlas

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
 const express = require('express')
-
+const mongoose = require('mongoose')
 const app = express()
 
 app.get('/true', async (req, res) => {
@@ -8,7 +9,14 @@ app.get('/true', async (req, res) => {
     res.status(200).json({'working' : 'true'});
 })
 
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(4000, () => {
+            console.log('listening on port 4000')
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
-app.listen(4000, () => {
-    console.log('listening on port 4000')
-})
+
